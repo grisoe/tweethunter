b5f9586db0901ec0e713938a90225fdc33c7d1b4
grisoe <srm.hdz@gmail.com>
2021-05-02 15:25:28 -0700

    Defined main structure.

--- main.py
+++ main.py
@@ -1,38 +1,25 @@
 import twint
 from datetime import date
 
-if __name__ == '__main__':
+
+def search_in_twitter():
     today = date.today()
 
     c = twint.Config()
-    c.Search = "bigtoken bot"
-    # Temp: for faster tests.
-    # c.Limit = 3
-    # For objects to be stored in twint.output.tweets_list
+    c.Search = "bigtoken"
+    c.Hide_output = True
     c.Store_object = True
-    # c.Since = str(today)
+    c.Since = str(today)
 
     twint.run.Search(c)
 
-    tweets = twint.output.tweets_list
-    # print('\n\n', tweets[-4].username)
+    return twint.output.tweets_list
 
-# c = twint.Config()
-# c.Username = "srmhdz"
-# # c.Store_object = True
-# c.User_full = True
-# c.Profile_full = True
-# c.Custom["tweet"] = ["id", "username", "link"]
-# c.Retweets = True
-# c.Hide_output = True
-# c.Lowercase = True
-# c.Store_json = True
-# c.Output = "none.json"
-#
-# # twint.run.Search(c)
-# # Returns more tweets.
-# twint.run.Profile(c)
-#
-# tweets = twint.output.tweets_list
-# for i in tweets:
-#     print(i.hashtags)
+
+def print_from_twitter(tweets):
+    for tweet in tweets:
+        print(tweet.username)
+
+
+if __name__ == '__main__':
+    print_from_twitter(search_in_twitter())
