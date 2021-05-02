4aea65eee3fae3bff8767cc09a5cd3123af0caa4
grisoe <srm.hdz@gmail.com>
2021-05-02 16:44:33 -0700

    Terms are loaded from a single JSON into two lists.

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import twint
+import json
 from datetime import date
+
+CONF_FILE = 'conf.json'
 
 
 def search_in_twitter():
     today = date.today()
 
     c = twint.Config()
-    c.Search = ""
+    c.Search = ''
     c.Hide_output = True
     c.Store_object = True
     c.Since = str(today)
@@ -21,5 +24,19 @@ def print_from_twitter(tweets):
         print(tweet.username)
 
 
+def get_conf_terms():
+    with open(CONF_FILE) as conf:
+        terms = json.load(conf)
+        in_twitter = []
+        in_tweets = []
+        for term in terms['inTwitter']:
+            in_twitter.append(term)
+        for term in terms['inTweets']:
+            in_tweets.append(term)
+    return in_twitter, in_tweets
+
+
 if __name__ == '__main__':
-    print_from_twitter(search_in_twitter())
+    # print_from_twitter(search_in_twitter())
+    print(get_conf_terms()[0])
+    print(get_conf_terms()[1])
